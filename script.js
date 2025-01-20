08a4c88f4ab6d8708343e59f36a6156f9d6cfd3c
JolugboE <jolugboesther@gmail.com>
2025-01-20 08:34:20 +0100

    i'm not going to give up

--- script.js
+++ script.js
@@ -100,11 +100,14 @@ const locations = [
   
   function buyWeapon() {
     if (gold >= 30) {
-        gold -= 30;
-        currentWeaponIndex++;
-    goldText.innerText = gold;
-    text.innerText = "You now have a new weapon.";
-  }
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: "
+    }
   }
   
   function fightSlime() {
