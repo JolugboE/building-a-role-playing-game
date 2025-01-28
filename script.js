6d2b2f5334d45e55f900da484ca9100d7a8c2a7e
JolugboE <jolugboesther@gmail.com>
2025-01-28 07:47:52 +0100

    I'm very close to the finish line

--- script.js
+++ script.js
@@ -256,5 +256,8 @@ const locations = [
     }
   function pick(guess){
     const numbers = [];
+    while (numbers.length < 10) {
+      numbers.push(Math.floor(Math.random() * 11))
+        }
   }
   
